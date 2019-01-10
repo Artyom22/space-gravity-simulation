66a418faebd2ff2e8d05281a5b394b5475480746
Artyom Amiryan <artyomamiryan2@gmail.com>
2019-01-10 18:05:39 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
